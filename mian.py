138b6877d0307ea3c47f6cf850ab6fb593163e00
v2ay <314051672@qq.com>
2018-06-23 13:43:06 +0800

    Update mian.py

--- mian.py
+++ mian.py
@@ -9,29 +9,29 @@ sys.path.append("")
 class TimeHandler(tornado.web.RequestHandler):
 
     def get(self):
-        f=open('\\time.config','r+').read()
+        f=open('time.config','r+').read()
         li=eval(f)
         hours=datetime.datetime.now().strftime('%H')
         riqi=datetime.datetime.now().strftime('%Y%m%d')
         li.append([str(riqi),int(hours)])
-        open('\\time.config','w').write(str(li))
+        open('time.config','w').write(str(li))
         XXX=[int(i[0]) for i in li]
         OOO=[i[1] for i in li]
         print(XXX)
         print(OOO)
         d1 = datetime.datetime.strptime(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), '%Y-%m-%d %H:%M:%S')
-        d2 = datetime.datetime.strptime(open('\\time.log','r+').read(), '%Y-%m-%d %H:%M:%S')
+        d2 = datetime.datetime.strptime(open('time.log','r+').read(), '%Y-%m-%d %H:%M:%S')
         delta = d1 - d2
         print(d1)
         bday=delta.days
         print(delta)
-        self.render('time_p.html',a=open('\\time.log','r+').read(),b=bday,c=d1,l0=str(XXX),l1=str(OOO))
+        self.render('time_p.html',a=open('time.log','r+').read(),b=bday,c=d1,l0=str(XXX),l1=str(OOO))
         
     def post(self,*args,**kwargs):
 
         d1 = datetime.datetime.strptime(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), '%Y-%m-%d %H:%M:%S')
 
-        d2 = datetime.datetime.strptime(open('\\time.log','r+').read(), '%Y-%m-%d %H:%M:%S')
+        d2 = datetime.datetime.strptime(open('time.log','r+').read(), '%Y-%m-%d %H:%M:%S')
 
         delta = d1 - d2
 
@@ -43,9 +43,9 @@ class TimeHandler(tornado.web.RequestHandler):
 
         print(a)
 
-        open('\\time.log','w+').write(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        open('time.log','w+').write(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
 
-        self.render("time.html",a=open('\\time.log','r+').read(),b=bday,c=d1)
+        self.render("time.html",a=open('time.log','r+').read(),b=bday,c=d1)
 
 
 
@@ -56,8 +56,8 @@ class MainHandler(tornado.web.RequestHandler):
         a=self.get_argument('username')
         b=self.get_argument('pwd')
         print(a+','+b)
-        if a=='admin' and b=='a18381801393':
-             li=[eval(i) for i in open('\qian.log','r+')][::-1]
+        if a=='admin' and b=='admin':
+             li=[eval(i) for i in open('qian.log','r+')][::-1]
              self.render('mob.html',f=li,q=a)
         else:
              self.write('<center><h1>登录失败:帐号或密码错误</h1></center>')
@@ -80,8 +80,8 @@ class AddHandler(tornado.web.RequestHandler):
                         dict={'fk':fk,'data':eval(data),'name':name,'url':url,'cookie':eval(cookie),'header':eval(header)}
 						
             print(dict)
-            f=open('\qian.config','a+')
-            f.write('\n')
+            f=open('qian.config','a+')
+            f.write('n')
             f.write(str(dict))
             f.close
             self.write('<center><h1>添加成功</h1></center>')
@@ -89,7 +89,7 @@ class AddHandler(tornado.web.RequestHandler):
             self.write('<center><h1>添加失败</h1></center>')
 class XianHandler(tornado.web.RequestHandler):
     def get(self):
-        list=[eval(i) for i in open('\qian.config','r+')][::-1]
+        list=[eval(i) for i in open('qian.config','r+')][::-1]
         self.render("xians.html",list=list)
     def post(self):
         pass
